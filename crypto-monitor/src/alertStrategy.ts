4a891cd73b873a32738aa251d357d3d568ba3589
Gabriel <gsmainiere1901@gmail.com>
2025-09-19 15:05:06 -0300

    feat: implementing alertStrategy

---
+++ crypto-monitor/src/alertStrategy.ts
@@ -0,0 +1,34 @@
+export interface AlertStrategy{
+    check(value : number) : Promise<void>;
+}
+
+export class ThresholdStrategy implements AlertStrategy{
+
+    constructor( private buy : number, private sell : number){}
+
+    async check(value : number) : Promise<void>{
+        if (value > this.buy) {
+            console.log(`ALERT! Low price to buy! (${value})`);
+        }
+        if (value > this.sell) {
+            console.log(`ALERT! High price to sell! (${value})`);
+        }
+    }
+}
+
+export class VariationStrategy implements AlertStrategy{
+    private lastPrice: number | null = null;
+
+    constructor(private percent : number){}
+
+    async check(value: number): Promise<void> {
+        
+        if (this.lastPrice) {
+            const difference = (Math.abs(value - this.lastPrice) / this.lastPrice) * 100;
+            if (difference > this.percent) {
+                console.log(`ALERT! ! Variation of ${difference.toFixed(2)}%`);
+            }
+        }
+        this.lastPrice = value;
+    }
+}
